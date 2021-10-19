5bea2371aa0c116907740fa4489f2f898bf01c20
Jan Ira Villacarlos <jmvillacarlos@up.edu.ph>
2021-10-19 12:12:28 +0800

    importing datasets

---
+++ Seagrass Assessment.R
@@ -0,0 +1,4 @@
+# importing datasets
+trantab <- read.delim("https://raw.githubusercontent.com/tbep-tech/seagrasstransect/master/docs/trantab.csv", sep=",")
+
+trannoctab <- read.delim("https://raw.githubusercontent.com/tbep-tech/seagrasstransect/master/docs/tranocctab.csv", sep=",")
